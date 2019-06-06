bf40505c52a65ddeeaa06a5d8783e34c20b3f367
Kristen Doucette <cosmicnoir90@gmail.com>
2019-06-06 17:01:30 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+
+function getResults(searchName){
+    const searchURL = `https://api.github.com/users/${searchName}/repos`;
+    console.log(searchURL);
+    // returns json from url get request
+    fetch(searchURL)
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson));
+}
+
+function handleForm() {
+    // responsible for handling form submission
+    $('#js-gitSearch').submit(event => {
+        event.preventDefault();
+        let searchName = $('#js-searchName').val();
+        console.log(searchName);
+        if (searchName === ""){
+            alert('Please enter a user search name!');
+        } else {
+            getResults(searchName);
+        }
+        console.log("handleForm ran");
+        
+
+    })
+}
+
+function handleDisplay() {
+    // displays the results and unhides the results seciton
+    $('.js-results').removeClass('hidden');
+    for (let i=0; i <responseJson.length; i++){
+        $('.js-results').append(`
+        <li><h3><a href="${responseJson[i].owner.url}">${response.Json[i].name}</a></h3></li>
+    `);
+    }
+}
+
+handleForm();
